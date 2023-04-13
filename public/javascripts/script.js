0eec38e4eadb2895df09f9960ddf1784e5581ade
Paul WOISARD <paulwoisard@gmail.com>
2023-04-14 00:27:13 +0200

    ça ressemble à une version stable !

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,7 @@ const socket = io();
 let msg = new SpeechSynthesisUtterance();
 let voice = undefined;
 const synth = window.speechSynthesis;
+let authorizeToSpeak = false;
 
 socket.on('marv', receive => {
     console.log(receive);
@@ -39,29 +40,108 @@ const voicesLoader = new Promise((resolve, reject) => {
     }
 });
 
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+
+let activeRecognition = false;
+
+const startButton = (event) => {
+    activeRecognition = !activeRecognition;
+    recognition = new SpeechRecognition();
+    console.log(event);
+    recognition = new SpeechRecognition();
+    recognition.lang = "fr-FR";
+    recognition.start();
+    const talk = document.getElementById('talk');
+    if (activeRecognition) {
+        talk.style.backgroundColor = '#fff';
+    } else {
+        recognition.stop();
+        talk.style.backgroundColor = '#f00';
+        return;
+    }
+
+    
+    ignore_onend = false;
+
+    start_timestamp = event.timeStamp;
+
+    recognition.onresult = (event) => {
+        if(event.isTrusted) {
+            const current = event.resultIndex;
+            const transcript = event.results[current][0].transcript;
+            const mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+            if(!mobileRepeatBug) {
+                const history = document.getElementById("history");
+                history.innerHTML += "\n_____________________________________________"
+                history.innerHTML += "\n\nUser : " + transcript;
+                socket.emit('marv', transcript);
+            }
+        }
+    };
+
+    recognition.onend = () => {
+        recognition.start();
+        activeRecognition = true;
+        talk.style.backgroundColor = '#fff';
+    }
+
+    recognition.onspeechend = async () => {
+        recognition.stop();
+        activeRecognition = false;
+        talk.style.backgroundColor = '#f00';
+    }
+
+    recognition.onerror = async (event) => {
+        recognition.stop();
+        activeRecognition = false;
+        talk.style.backgroundColor = '#f00';
+    }
+}
+
 voicesLoader.then(voices => {
     voice = voices.find(el => el.name === "Microsoft Paul - French (France)");
     if(!voice) voice = voices.find(el => el.name === "Microsoft Hortense Desktop - French");
 });
 
 const syntheseVocale = text => {
-    return new Promise((resolve, reject) => {
-        const toSpeak = new SpeechSynthesisUtterance(text);
-        toSpeak.lang = "fr-FR";
-        toSpeak.rate = 1;
-        toSpeak.voice = voice;
-        toSpeak.addEventListener("end", () => {
-            resolve();
+    if (authorizeToSpeak) {
+        return new Promise((resolve, reject) => {
+            const toSpeak = new SpeechSynthesisUtterance(text);
+            toSpeak.lang = "fr-FR";
+            toSpeak.rate = 1;
+            toSpeak.voice = voice;
+            toSpeak.addEventListener("end", () => {
+                resolve();
+            });
+            
+            synth.speak(toSpeak);
+            talk(true);
+    
+            toSpeak.onend = (event) => {
+                console.log(event)
+                talk(false);
+            };
         });
-        talk(true);
-        synth.speak(toSpeak);
-
-        toSpeak.onend = (event) => {
-            console.log(event)
-            talk(false);
-        };
-    });
+    }
 } 
+
+const toggleSynth = (event) => {
+    const speech = document.getElementById('speech');
+    if (authorizeToSpeak) {
+        synth.cancel();
+        talk(false);
+        speech.style.backgroundColor = '#f00';
+        authorizeToSpeak = false;
+        return;
+    } else {
+        speech.style.backgroundColor = '#fff';
+        authorizeToSpeak = true;
+        return;
+    }
+}
+
+
 
 
 
@@ -74,6 +154,8 @@ const talk = (speak) => {
     }
 }
 
+
+
 // Execute a function when the user presses a key on the keyboard
 const preventMoving = (event) => {
     // Get the input field
@@ -85,7 +167,6 @@ const preventMoving = (event) => {
         // Quelque chose à faire avec l'event
         console.log(input.value);
         const history = document.getElementById("history");
-        bot.src = 'images/botavatar.gif';
         history.innerHTML += "\n_____________________________________________"
         history.innerHTML += "\n\nUser : " + input.value;
         socket.emit('marv', input.value);   
